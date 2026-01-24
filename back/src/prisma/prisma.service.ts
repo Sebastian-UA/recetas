cc3d4f6142bf39bf0a969e636630e73a80981342
Sebastian-UA <sebaua2002@gmail.com>
2026-01-24 02:10:42 -0300

    .

--- back/src/prisma/prisma.service.ts
+++ back/src/prisma/prisma.service.ts
@@ -2,7 +2,14 @@ import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit {
+
+  constructor() {
+    super(); // 👈 así, vacío
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
