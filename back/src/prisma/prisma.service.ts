58d657f7ebb8fac2585d89aeb50222794b952ae4
Sebastian-UA <sebaua2002@gmail.com>
2026-01-24 01:41:10 -0300

    Update prisma.service.ts

--- back/src/prisma/prisma.service.ts
+++ back/src/prisma/prisma.service.ts
@@ -1,21 +1,16 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
-import { PrismaBetterSqlite3  } from '@prisma/adapter-better-sqlite3';
 
 @Injectable()
 export class PrismaService
   extends PrismaClient
-  implements OnModuleInit {
-
-  constructor() {
-    super({
-      adapter: new PrismaBetterSqlite3 ({
-        url: process.env.DATABASE_URL as string,
-      }),
-    });
-  }
+  implements OnModuleInit, OnModuleDestroy {
 
   async onModuleInit() {
     await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
   }
 }
